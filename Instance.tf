5f3f8b45e48f60eb3dbf20675bfa9430de7c8b09
Abhishek Maheshwari <111350540+Abhibabe@users.noreply.github.com>
2025-06-05 15:39:44 +0530

    Add files via upload

--- Instance.tf
+++ Instance.tf
@@ -1,1 +1,13 @@
+resource "aws_instance" "app_server_1" {
+  ami           = "ami-0c02fb55956c7d316" 
+  instance_type          = "t2.micro"
+  subnet_id     = aws_subnet.subnet_az1.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+}
 
+resource "aws_instance" "app_server_2" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.subnet_az2.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+}
--- Network.tf
+++ Network.tf
@@ -4,17 +4,12 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "subnet_az1" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1"
+  availability_zone = "us-east-1a"
 }
 
-  tags = {
-    Name = "Public Subnet"
+resource "aws_subnet" "subnet_az2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
 }
-resource "aws_subnet" "privatesub" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = var.cidr_private
 
-  tags = {
-    Name = "Pri Subnet"
-  }
-}
